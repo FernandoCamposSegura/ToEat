e1d437399d24969717a6d7aac06504a5aa59d2e9
FernandoCamposSegura <campossegura19@gmail.com>
2023-01-30 19:59:12 +0100

    Fragment implementation

--- app/src/main/java/com/fcampos/toeat/MainActivity.java
+++ app/src/main/java/com/fcampos/toeat/MainActivity.java
@@ -1,14 +1,52 @@
 package com.fcampos.toeat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.fcampos.toeat.databinding.ActivityMainBinding;
+import com.fcampos.toeat.fragment.FavFragment;
+import com.fcampos.toeat.fragment.HomeFragment;
+import com.fcampos.toeat.fragment.ProfileFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.fav:
+                    replaceFragment(new FavFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
